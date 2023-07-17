8b02e786522f4c99cedeecac34bce1acf3b3d71a
Achyuta Pradhan <achyuta4003@gmail.com>
2023-07-17 22:24:56 +0530

    modallProblem

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-const div = document.querySelector("div")
-const form = document.querySelector("form")
-const button = document.querySelector("button")
+//Q: Create a modal which closes by clicking on negative space
 
+const container = document.querySelector(".moodalContainer")
+const button = document.querySelector(".modalButton")
 
-div.addEventListener("click", function () {
-    alert("div")
+button.addEventListener("click", () => {
+    toggleModal(true)
+})
+container.addEventListener("click", (e) => {
+    console.log(e.target.className);
+    if (e.target.className === "moodalContainer") {
+
+        toggleModal(false)
+    }
 })
 
-
-form.addEventListener("click", function () {
-    alert("form")
-})
-
-
-button.addEventListener("click", function () {
-    alert("button")
-})
+function toggleModal(toggle) {
+    container.style.display = toggle ? "grid" : "none"
+}
