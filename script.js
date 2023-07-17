52892943e18e95cc44797d731dc3f0f790d60452
Achyuta Pradhan <achyuta4003@gmail.com>
2023-07-17 21:17:11 +0530

    event.target terminaology

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
+// Q: event.target vs event.currentTarget vs this.target
 const div = document.querySelector("div")
 const form = document.querySelector("form")
 const button = document.querySelector("button")
 
 
-div.addEventListener("click", function () {
-    alert("div")
-})
+div.addEventListener("click", func)
 
 
-form.addEventListener("click", function () {
-    alert("form")
-})
+form.addEventListener("click", func)
 
 
-button.addEventListener("click", function () {
-    alert("button")
-})
+button.addEventListener("click", func)
+
+//--> event.currentTarget
+
+// function func(event) {
+//     alert("event.currentTarget" + event.currentTarget.tagName)
+// }
+
+//--.event.currentTarget
+// function func(event) {
+//     alert("event.target" + event.target.tagName)
+// }
+
+
+// --> this.tagname
+function func(event) {
+    alert("this.target" + this.tagName)
+}
