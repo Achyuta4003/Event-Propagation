2f166b8e2505253c490b21806d0091e5d8632079
Achyuta Pradhan <achyuta4003@gmail.com>
2023-07-17 21:32:09 +0530

    event capturing

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ const button = document.querySelector("button")
 
 div.addEventListener("click", function () {
     alert("div")
-})
+}, true)
 
 
 form.addEventListener("click", function () {
     alert("form")
-})
+}, true)
 
 
 button.addEventListener("click", function () {
     alert("button")
-})
+}, true)
