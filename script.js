2b9473bbf593cdd014c3eea31ad774e437f66c3d
Achyuta Pradhan <achyuta4003@gmail.com>
2023-07-17 20:09:22 +0530

    debounce pollyfill Implementation

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// Q:create a button UI and add throttle as follow
-//     -->Show button press X time, Every time button is pressed
-//     -->Increase Trigger Y time, count after 800ms of throttle
+// Q:Create Debounce() Pollyfill Implementation 
 
 
 const btn = document.querySelector(".increment_btn");
@@ -11,12 +9,26 @@ const count = document.querySelector(".increment_count");
 let pressCount = 0;
 let triggerCount = 0;
 
-const throttlingCount = _.throttle(() => {
+
+// Debounce() Pollyfill
+const myDebounce = (cb, d) => {
+    let timer;
+    return function (...args) {
+
+        if (timer) clearTimeout(timer);
+
+        timer = setTimeout(() => {
+            cb(...args)
+        }, d)
+    }
+}
+
+const debounceCount = myDebounce(() => {
     count.innerHTML = ++triggerCount;
 }, 800)
 
 
 btn.addEventListener('click', () => {
     btnPress.innerHTML = ++pressCount;
-    throttlingCount()
+    debounceCount()
 })
